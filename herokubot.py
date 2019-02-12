50afd09a00edc90b2e5442eb22a1725c2d682a97
mhrimaz <mhrimaz@yahoo.com>
2019-02-13 01:41:41 +0330

    fix property

--- herokubot.py
+++ herokubot.py
@@ -31,7 +31,9 @@ def isEnglish(text):
     return countEnglishLetters/len(str) >0.5
 
 def processText(bot, update):
-    username = update.message.from_user.username
+    user = update.message.from_user
+    print(user, "  " , update)
+    username = user['username']
     logger.debug('user name'+ username) 
     logger.debug('update.message.text '+ update.message.text + 'isenglish: '+isEnglish(update.message.text))     
     if isEnglish(update.message.text):
