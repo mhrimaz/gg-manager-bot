c2305231262970bca6ec43994de521126f6a9427
mhrimaz <mhrimaz@yahoo.com>
2019-02-12 23:15:07 +0330

    why?

--- herokubot.py
+++ herokubot.py
@@ -43,9 +43,9 @@ if __name__ == "__main__":
     dp.add_error_handler(error)
 
     # Start the webhook
-    updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
-    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+    #updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
+    #updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
 	
 	# Start the Bot
-    #updater.start_polling()
+    updater.start_polling()
     updater.idle()
