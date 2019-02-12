1594978fbda3b52c2053d98cbe1d053a6e023955
mhrimaz <mhrimaz@yahoo.com>
2019-02-12 23:35:11 +0330

    why?

--- herokubot.py
+++ herokubot.py
@@ -12,8 +12,8 @@ def randomgame(bot, update):
     update.message.reply_text("GG R6")
 
 
-def echo(bot, update):
-    update.effective_message.reply_text(update.effective_message.text)
+#def echo(bot, update):
+    #update.effective_message.reply_text(update.effective_message.text)
 
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
@@ -28,8 +28,7 @@ if __name__ == "__main__":
     PORT = os.environ.get('PORT')
 
     # Enable logging
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                        level=logging.INFO)
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
     logger = logging.getLogger(__name__)
 
     # Set up the Updater
@@ -37,15 +36,15 @@ if __name__ == "__main__":
     dp = updater.dispatcher
     # Add handlers
     dp.add_handler(CommandHandler('start', start))
-	#dp.add_handler(CommandHandler('roll', roll))
-	dp.add_handler(CommandHandler('randomgame', randomgame))
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    #dp.add_handler(CommandHandler('roll', roll))
+    dp.add_handler(CommandHandler('randomgame', randomgame))
+    #dp.add_handler(MessageHandler(Filters.text, echo))
     dp.add_error_handler(error)
 
     # Start the webhook
-    #updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
-    #updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
-	
-	# Start the Bot
-    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
+    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+    
+    # Start the Bot
+    #updater.start_polling()
     updater.idle()
