c9bfd500155ffdb4052246e79a7b91450fdd4b9c
Hossein Rimaz <mhrimaz@yahoo.com>
2019-02-17 19:29:45 +0330

    fix feature forgive

--- herokubot.py
+++ herokubot.py
@@ -38,6 +38,8 @@ def unknown(bot, update):
     user = update.effective_user
     userID = user.id
     command = update.effective_message.text
+    admins = set([admin.user.id for admin in bot.get_chat_administrators(
+            update.effective_message.chat.id)])
     print(userID , " => ", str(admins))
     print("user :",command[command.find('@')+1:])
     if((userID in admins) and (command.startswith("/forgive"))):
