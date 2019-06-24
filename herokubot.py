eaa4460e6ea75c0732f6af3ab984a7db3975f08a
Hossein Rimay <mhrimaz@yahoo.com>
2019-06-24 22:43:53 +0430

    fix bug

--- herokubot.py
+++ herokubot.py
@@ -107,7 +107,7 @@ def unknown(bot, update):
     user = update.effective_user
     userID = user.id
     command = update.effective_message.text
-    if(not (update.effective_message.chtt.id == GROUP_ID)):
+    if(not (update.effective_message.chat.id == GROUP_ID)):
         return
     admins = set([admin.user.id for admin in bot.get_chat_administrators(GROUP_ID)])
     print(userID, " => ", str(admins))
